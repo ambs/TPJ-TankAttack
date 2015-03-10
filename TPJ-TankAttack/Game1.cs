6760de02d7bd0099d73770fe907038acd79a5989
Alberto Simões <ambs@cpan.org>
2015-03-10 15:51:57 +0000

    Camera e Sprite Basica

---
+++ TPJ-TankAttack/Game1.cs
@@ -0,0 +1,84 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace TPJ_TankAttack
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Sprite tank;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferHeight = 700;
+            graphics.PreferredBackBufferWidth = 600;
+            graphics.ApplyChanges();
+
+            Camera.SetGraphicsDeviceManager(graphics);
+            Camera.SetTarget(Vector2.Zero);
+            Camera.SetWorldWidth(5);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            tank = new Sprite(Content, "tank_body");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            tank.Draw(spriteBatch);
+            spriteBatch.End();
+        
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TPJ-TankAttack/Sprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TPJ_TankAttack
+{
+    class Sprite
+    {
+        private Texture2D image;
+        private Vector2 position;
+        private Vector2 size;
+        private float rotation;
+
+        public Sprite(ContentManager contents, String assetName)
+        {
+            this.rotation = 0f;
+            this.size = new Vector2(1f, 1f);
+            this.position = Vector2.Zero;
+            this.image = contents.Load<Texture2D>(assetName);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle pos = Camera.WorldSize2PixelRectangle(this.position, this.size);
+            spriteBatch.Draw(this.image, pos, Color.White);
+        }
+    }
+}
